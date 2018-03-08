b25fcb25d2625e024f1607b8538cf2dd0b5d347c
Ernesto Hernandez <h_ernesto@ymail.com>
2018-03-07 21:57:44 -0800

    third try

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,5 @@ const apiRoutes = require("./users");
 /** Note that this adds the final part to the path url */
 /** This changes the /suite and suite/:api paths in api folder to */
 /** /api/suite and /api/suite/:id */
-router.use("/", function(req, res){
-  res.render("index.html");
-});
+router.use("/");
 router.use("/api", apiRoutes);
