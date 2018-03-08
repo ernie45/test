13a41f3f5f06a4df82003e7e20a6496b81c1d91e
Ernesto Hernandez <h_ernesto@ymail.com>
2018-03-07 21:45:29 -0800

    copied everything

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,5 @@ const apiRoutes = require("./users");
 /** Note that this adds the final part to the path url */
 /** This changes the /suite and suite/:api paths in api folder to */
 /** /api/suite and /api/suite/:id */
-router.get("/", function(req, res){
-    console.log("here");
-});
+
 router.use("/api", apiRoutes);
